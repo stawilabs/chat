74b2677dd984736b61ce552cc560e7c4dd612694
Peter Bwire <bwire517@gmail.com>
2026-04-09 17:58:16 +0300

    fix: export sentry stub symbol for Windows DLL import library generation
    MSVC only generates a .lib import library alongside a .dll when the DLL
    exports at least one symbol. Without this, the Flutter runner linker
    fails with LNK1104: cannot open file 'sentry.lib'.

--- ui/third_party/sentry_flutter/sentry-native/sentry_stub.cpp
+++ ui/third_party/sentry_flutter/sentry-native/sentry_stub.cpp
@@ -1,1 +1,7 @@
-void sentry_flutter_native_stub() {}
+#ifdef _WIN32
+#define SENTRY_EXPORT __declspec(dllexport)
+#else
+#define SENTRY_EXPORT __attribute__((visibility("default")))
+#endif
+
+extern "C" SENTRY_EXPORT void sentry_flutter_native_stub() {}
