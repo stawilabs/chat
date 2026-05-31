ca08f2758d67ee4c1a6f2d40b3865d960d159d36
Peter Bwire <bwire517@gmail.com>
2026-05-31 09:28:25 +0300

    test(ui): drop unused drift import in pending_job_repository_test

--- ui/test/unit/core/sync/pending_job_repository_test.dart
+++ ui/test/unit/core/sync/pending_job_repository_test.dart
@@ -1,4 +1,3 @@
-import 'package:drift/drift.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:stawi/core/db/database.dart';
 import 'package:stawi/core/sync/pending_job.dart' as domain;
